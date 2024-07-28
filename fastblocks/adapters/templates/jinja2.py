d34cf80055ad78d5b6e5759a741a3b9a3669b63a
lesleslie <les@wedgwoodwebworks.com>
2024-07-28 02:35:11 -0700

    use passwordless cache

--- fastblocks/adapters/templates/jinja2.py
+++ fastblocks/adapters/templates/jinja2.py
@@ -319,7 +319,7 @@ class Templates(TemplatesBase):
             )
         bytecode_cache = AsyncRedisBytecodeCache(
             prefix=self.config.app.name,
-            password=self.config.cache.password.get_secret_value(),
+            # password=self.config.cache.password.get_secret_value(),
             host=self.config.cache.host.get_secret_value(),
             port=self.config.cache.port,
             db=self.config.templates.cache_db,
