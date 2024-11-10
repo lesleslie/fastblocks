7f9b733b65d115666b991fc2212435e45d092ffb
lesleslie <les@wedgwoodwebworks.com>
2024-11-10 01:15:27 -0800

    update crackerjack - python3.13

--- fastblocks/actions/minify.py
+++ fastblocks/actions/minify.py
@@ -1,26 +1,20 @@
-from subprocess import run
-
-from htmlmin import minify as min_html
-from jsmin import jsmin as min_js
+from minify_html import minify as min_html  # type: ignore
+from rcssmin import cssmin as min_css
+from rjsmin import jsmin as min_js
 
 
 class Minify:
     @staticmethod
-    def js(js: str) -> str:
+    def js(js: str) -> bytearray | bytes | str:
         return min_js(js)
 
     @staticmethod
     def html(html: str) -> str:
-        return min_html(html)
+        return min_html(html, minify_css=True, minify_js=True)
 
     @staticmethod
-    def scss(css: str) -> str:
-        return str(
-            run(  # noqa
-                ["sass", "--no-source-map", "--style=compressed", "--stdin", css],
-                capture_output=True,
-            ).stdout
-        )
+    def css(css: str) -> bytearray | bytes | str:
+        return min_css(css)
 
 
 minify = Minify()
