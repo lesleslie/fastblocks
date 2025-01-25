45dab826b0ca83bbd8be8e4d4d57a0efda81e563
lesleslie <les@wedgwoodwebworks.com>
2025-01-25 11:35:53 -0800

    update exception handler

--- fastblocks/exceptions.py
+++ fastblocks/exceptions.py
@@ -8,14 +8,14 @@ from starlette.responses import PlainTextResponse
 async def handle_exception(request: HtmxRequest, exc: HTTPException):
     responses = {404: "Content not found", 500: "Server error"}
     templates = depends.get(import_adapter("templates"))
-    exc.status_code = 500 if not exc.status_code else exc.status_code
+    status_code = getattr(exc, "status_code", 500)
     if request.scope["htmx"]:
         return PlainTextResponse(
-            content=responses[exc.status_code], status_code=exc.status_code
+            content=responses[status_code], status_code=status_code
         )
     return await templates.app.render_template(
         request,
         "index.html",
-        status_code=exc.status_code,
-        context=dict(page=str(exc.status_code)),
+        status_code=status_code,
+        context=dict(page=str(status_code)),
     )
